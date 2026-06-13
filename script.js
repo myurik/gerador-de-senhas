3cf218f7ad792a36df40d0cb0e3c0fc9d8f21c38
Matheus Yuri Miguel <msyf.business@gmail.com>
2026-06-13 06:09:16 -0300

    Implementa logica de geracao de senha (obterCaracteresDisponiveis e gerarSenha)

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputSenha = document.getElementById("senha-gerada")
+const btnCopiar = document.getElementById("btn-copiar")
+const sliderTamanho = document.getElementById("tamanho")
+const valorTamanho = document.getElementById("valor-tamanho")
+const checkMaiusculas = document.getElementById("maiusculas")
+const checkMinusculas = document.getElementById("minusculas")
+const checkNumeros = document.getElementById("numeros")
+const checkSimbolos = document.getElementById("simbolos")
+const btnGerar = document.getElementById("btn-gerar")
+
+const LETRAS_MAIUSCULAS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LETRAS_MINUSCULAS = "abcdefghijklmnopqrstuvwxyz";
+const NUMEROS = "0123456789";
+const SIMBOLOS = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+function obterCaracteresDisponiveis(){
+    let caracteres = ""
+
+    if (checkMaiusculas.checked === true) {
+        caracteres += LETRAS_MAIUSCULAS
+    }
+    if (checkMinusculas.checked === true) {
+        caracteres += LETRAS_MINUSCULAS
+    }
+    if (checkNumeros.checked === true) {
+        caracteres += NUMEROS
+    }
+    if (checkSimbolos.checked === true) {
+        caracteres += SIMBOLOS
+    }
+    return caracteres;
+}
+
+function gerarSenha(){
+    let caracteres = obterCaracteresDisponiveis()
+    let tamanho = Number(sliderTamanho.value)
+    let senha = ""
+
+    for (i = 0; i < tamanho; i++) {
+        index = Math.floor(Math.random() * caracteres.length);
+        senha += caracteres[index];
+    }
+
+    return senha;
+}
